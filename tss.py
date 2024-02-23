2f220232349e46ee803da403786803102e849008
NaimChowdhury <naimul.chowdhury15@ncf.edu>
2024-02-23 12:39:26 +0800

    Data load-in and debug.

--- tss.py
+++ tss.py
@@ -1,1 +1,14 @@
 ## First Line
+
+# Import pandas library
+import pandas as pd
+
+# Load dataset
+data = pd.read_csv('../dat/C00368/data.csv')
+
+# Print headers of columns
+print("Column Headers:", data.columns)
+
+# Test: Print 5 columns
+print("First five rows:")
+print(data.head())
