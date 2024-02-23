8b118f3813db9e1c23fe427dc219704fc717d06b
NaimChowdhury <naimul.chowdhury15@ncf.edu>
2024-02-23 13:43:04 +0800

    Data organization and instantiation

--- tss.py
+++ tss.py
@@ -3,12 +3,21 @@
 # Import pandas library
 import pandas as pd
 
-# Load dataset
-data = pd.read_csv('../dat/C00368/data.csv')
+# Load datasets
+dtafile_law_society = '../dat/C00368/data.csv'
+dtafile_summary_2020 = '../dat/C00369/tscs2020q1.dta'
+dtafile_family_2020 = '../dat/C00390/tscs2021q1.dta'
 
-# Print headers of columns
-print("Column Headers:", data.columns)
+
+df_law_society = pd.read_csv(dtafile_law_society)
+df_summary_2020 = pd.read_stata(dtafile_summary_2020)
+df_family_2020 = pd.read_stata(dtafile_family_2020)
+
+
+# Print headers of columns of Law and Society
+print("Column Headers:", df_law_society.columns)
 
 # Test: Print 5 columns
 print("First five rows:")
-print(data.head())
+print(df_law_society.head())
+
