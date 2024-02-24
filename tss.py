8e71137fb6cfd670ab5ee4f965cbdd3d38e52761
NaimChowdhury <naimul.chowdhury15@ncf.edu>
2024-02-24 17:14:14 +0800

    Fixing encoding issues with 2013 dataset.

--- tss.py
+++ tss.py
@@ -4,28 +4,19 @@
 import pandas as pd
 
 # Load datasets
-#	dtafile_law_society = '../dat/C00368/data.csv'
-#	dtafile_summary_2020 = '../dat/C00369/tscs2020q1.dta'
-#	dtafile_family_2020 = '../dat/C00390/tscs2021q1.dta'
-	dtafile_national_2013 = '../dat/0604Q2/tscs2013q2.dta'
-	dtafile_2003 = '../dat/C00111_2/tscs2003q2.dta'
+#dtafile_law_society = '../dat/C00368/data.csv'
+#dtafile_summary_2020 = '../dat/C00369/tscs2020q1.dta'
+#dtafile_family_2020 = '../dat/C00390/tscs2021q1.dta'
+dtafile_national_2013 = '../dat/0604Q2/tscs2013q2.dta'
+dtafile_2003 = '../dat/C00111_2/tscs2003q2.dta'
+
+#df_law_society = pd.read_csv(dtafile_law_society)
+#df_summary_2020 = pd.read_stata(dtafile_summary_2020)
+#df_family_2020 = pd.read_stata(dtafile_family_2020)
+df_national_2013 = pd.read_stata(dtafile_national_2013)
 
 
-#	df_law_society = pd.read_csv(dtafile_law_society)
-#	df_summary_2020 = pd.read_stata(dtafile_summary_2020)
-#	df_family_2020 = pd.read_stata(dtafile_family_2020)
-	df_national_2013 = pd.read_stata(dtafile_national_2013)
-	df_2003 = pd.read_stata(dtafile_2003)
+# Testing converting the encoding for a single column
+df_national_2013['zip'] = df_national_2013['zip'].apply(lambda x: x.encode('latin-1').decode('big5-tw'))
 
-# Data exploration
-
-# Print headers of columns
-	# print("Column Headers:", df_law_society.columns)
-
-
-# Test: Print 5 columns 
-#	print("First five rows:")
-#	print(df_law_society.head())
-#	print(df_summary_2020.head())
-#	print(df_family_2020.head())
 
